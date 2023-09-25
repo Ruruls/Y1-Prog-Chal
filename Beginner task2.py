38e45c452f2e0d41ecd3afe351e5e2ff0f593860
Ruruls <112417908+Ruruls@users.noreply.github.com>
2023-09-25 10:44:18 +0100

    Add files via upload

---
+++ Beginner task2.py
@@ -0,0 +1,7 @@
+hourspernight = input("How many hours per night do you sleep?")
+hoursperweek = int(hourspernight)* 7 
+print("You sleep",hoursperweek,"hours per week")
+hourspermonth = float(hoursperweek) * 4.35
+print ("You sleep",hourspermonth,"hours per month")
+dayspermonth = float(hourspermonth) / 24 
+print ("This equates to",dayspermonth,"days per month")
